5e8a3eb5a3dc2676e073d462c12ac0e91cf3c645
costarc <ronivon.costa@gmail.com>
2023-03-14 15:30:18 +0000

    Updated rom_to_vhd.py

--- Reference Designs/Altera DE0 Project/Multi_Cartridge/Tools/rom_to_vhd.py
+++ Reference Designs/Altera DE0 Project/Multi_Cartridge/Tools/rom_to_vhd.py
@@ -6,6 +6,7 @@ fname = sys.argv[1]
 
 #component=fname.split('.')[0]
 component='rom'
+size = len(open(fname,"rb").read())-1
 
 print("library IEEE;")
 print("use IEEE.std_logic_1164.all;")
@@ -20,15 +21,15 @@ print("	);")
 print("end %s;" % component)
 print("")
 print("architecture rtl of %s is" % component)
-print("type ROM is array (0 to 32767) of std_logic_vector(7 downto 0);")
-print("constant galaga : rom := (")
+print("type MEMORY is array ( 0 to",size,") of std_logic_vector(7 downto 0);")
+print("constant rom : MEMORY := (")
 addr = 0
 f = open(fname,"rb")
 byte = True
 while byte:
     byte = f.read(1).hex()
     if (byte):
-        if addr < 32767:
+        if addr < size:
             print("    x\"%s\"," % (byte))
         else:
             print("    x\"%s\"" % (byte))
@@ -40,7 +41,7 @@ print("")
 print("process (cs)")
 print("begin")
 print("if rising_edge(cs) then")
-print("    D <= galaga(to_integer(unsigned(A)));")
+print("    D <= rom(to_integer(unsigned(A)));")
 print("end if;")
 print("end process;")
 print("end;")
