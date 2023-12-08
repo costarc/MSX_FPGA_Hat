20b69366537f7265c06bf5b24d5fdd8772897f14
Roni <ronivon.costa@gmail.com>
2023-12-08 19:44:16 +0000

    Changed SPI clock to PLL

--- Reference Designs/Altera DE1  Project/SDMapper/Clock_25MHz.vhd
+++ Reference Designs/Altera DE1  Project/SDMapper/Clock_25MHz.vhd
@@ -4,7 +4,7 @@
 -- MODULE: altpll 
 
 -- ============================================================
--- File Name: Clock_25MHz.vhd
+-- File Name: clock_25mhz.vhd
 -- Megafunction Name(s):
 -- 			altpll
 --
@@ -39,13 +39,14 @@ USE ieee.std_logic_1164.all;
 LIBRARY altera_mf;
 USE altera_mf.all;
 
-ENTITY Clock_25MHz IS
+ENTITY clock_25mhz IS
 	PORT
 	(
+		areset		: IN STD_LOGIC  := '0';
 		inclk0		: IN STD_LOGIC  := '0';
 		c0		: OUT STD_LOGIC 
 	);
-END Clock_25MHz;
+END clock_25mhz;
 
 
 ARCHITECTURE SYN OF clock_25mhz IS
@@ -114,6 +115,7 @@ ARCHITECTURE SYN OF clock_25mhz IS
 		port_extclk3		: STRING
 	);
 	PORT (
+			areset	: IN STD_LOGIC ;
 			clk	: OUT STD_LOGIC_VECTOR (5 DOWNTO 0);
 			inclk	: IN STD_LOGIC_VECTOR (1 DOWNTO 0)
 	);
@@ -136,11 +138,11 @@ BEGIN
 		compensate_clock => "CLK0",
 		inclk0_input_frequency => 20000,
 		intended_device_family => "Cyclone II",
-		lpm_hint => "CBX_MODULE_PREFIX=Clock_25MHz",
+		lpm_hint => "CBX_MODULE_PREFIX=clock_25mhz",
 		lpm_type => "altpll",
 		operation_mode => "NORMAL",
 		port_activeclock => "PORT_UNUSED",
-		port_areset => "PORT_UNUSED",
+		port_areset => "PORT_USED",
 		port_clkbad0 => "PORT_UNUSED",
 		port_clkbad1 => "PORT_UNUSED",
 		port_clkloss => "PORT_UNUSED",
@@ -182,6 +184,7 @@ BEGIN
 		port_extclk3 => "PORT_UNUSED"
 	)
 	PORT MAP (
+		areset => areset,
 		inclk => sub_wire3,
 		clk => sub_wire0
 	);
@@ -210,7 +213,7 @@ END SYN;
 -- Retrieval info: PRIVATE: CUR_DEDICATED_CLK STRING "c0"
 -- Retrieval info: PRIVATE: CUR_FBIN_CLK STRING "c0"
 -- Retrieval info: PRIVATE: DEVICE_SPEED_GRADE STRING "7"
--- Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "1"
+-- Retrieval info: PRIVATE: DIV_FACTOR0 NUMERIC "2"
 -- Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
 -- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "25.000000"
 -- Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
@@ -237,8 +240,8 @@ END SYN;
 -- Retrieval info: PRIVATE: MIRROR_CLK0 STRING "0"
 -- Retrieval info: PRIVATE: MULT_FACTOR0 NUMERIC "1"
 -- Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
--- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "25.00000000"
--- Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "100.00000000"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "0"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_UNIT0 STRING "MHz"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_FEATURE_ENABLED STRING "0"
 -- Retrieval info: PRIVATE: PHASE_RECONFIG_INPUTS_CHECK STRING "0"
@@ -246,7 +249,7 @@ END SYN;
 -- Retrieval info: PRIVATE: PHASE_SHIFT_STEP_ENABLED_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PHASE_SHIFT_UNIT0 STRING "deg"
 -- Retrieval info: PRIVATE: PLL_ADVANCED_PARAM_CHECK STRING "0"
--- Retrieval info: PRIVATE: PLL_ARESET_CHECK STRING "0"
+-- Retrieval info: PRIVATE: PLL_ARESET_CHECK STRING "1"
 -- Retrieval info: PRIVATE: PLL_AUTOPLL_CHECK NUMERIC "1"
 -- Retrieval info: PRIVATE: PLL_ENA_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PLL_ENHPLL_CHECK NUMERIC "0"
@@ -256,7 +259,7 @@ END SYN;
 -- Retrieval info: PRIVATE: PLL_PFDENA_CHECK STRING "0"
 -- Retrieval info: PRIVATE: PLL_TARGET_HARCOPY_CHECK NUMERIC "0"
 -- Retrieval info: PRIVATE: PRIMARY_CLK_COMBO STRING "inclk0"
--- Retrieval info: PRIVATE: RECONFIG_FILE STRING "Clock_25MHz.mif"
+-- Retrieval info: PRIVATE: RECONFIG_FILE STRING "clock_25mhz.mif"
 -- Retrieval info: PRIVATE: SACN_INPUTS_CHECK STRING "0"
 -- Retrieval info: PRIVATE: SCAN_FEATURE_ENABLED STRING "0"
 -- Retrieval info: PRIVATE: SELF_RESET_LOCK_LOSS STRING "0"
@@ -286,7 +289,7 @@ END SYN;
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altpll"
 -- Retrieval info: CONSTANT: OPERATION_MODE STRING "NORMAL"
 -- Retrieval info: CONSTANT: PORT_ACTIVECLOCK STRING "PORT_UNUSED"
--- Retrieval info: CONSTANT: PORT_ARESET STRING "PORT_UNUSED"
+-- Retrieval info: CONSTANT: PORT_ARESET STRING "PORT_USED"
 -- Retrieval info: CONSTANT: PORT_CLKBAD0 STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: PORT_CLKBAD1 STRING "PORT_UNUSED"
 -- Retrieval info: CONSTANT: PORT_CLKLOSS STRING "PORT_UNUSED"
@@ -329,16 +332,18 @@ END SYN;
 -- Retrieval info: USED_PORT: @clk 0 0 6 0 OUTPUT_CLK_EXT VCC "@clk[5..0]"
 -- Retrieval info: USED_PORT: @extclk 0 0 4 0 OUTPUT_CLK_EXT VCC "@extclk[3..0]"
 -- Retrieval info: USED_PORT: @inclk 0 0 2 0 INPUT_CLK_EXT VCC "@inclk[1..0]"
+-- Retrieval info: USED_PORT: areset 0 0 0 0 INPUT GND "areset"
 -- Retrieval info: USED_PORT: c0 0 0 0 0 OUTPUT_CLK_EXT VCC "c0"
 -- Retrieval info: USED_PORT: inclk0 0 0 0 0 INPUT_CLK_EXT GND "inclk0"
+-- Retrieval info: CONNECT: @areset 0 0 0 0 areset 0 0 0 0
 -- Retrieval info: CONNECT: @inclk 0 0 1 1 GND 0 0 0 0
 -- Retrieval info: CONNECT: @inclk 0 0 1 0 inclk0 0 0 0 0
 -- Retrieval info: CONNECT: c0 0 0 0 0 @clk 0 0 1 0
--- Retrieval info: GEN_FILE: TYPE_NORMAL Clock_25MHz.vhd TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Clock_25MHz.ppf TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Clock_25MHz.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Clock_25MHz.cmp FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Clock_25MHz.bsf FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL Clock_25MHz_inst.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL clock_25mhz.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL clock_25mhz.ppf TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL clock_25mhz.inc FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL clock_25mhz.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL clock_25mhz.bsf TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL clock_25mhz_inst.vhd TRUE
 -- Retrieval info: LIB_FILE: altera_mf
 -- Retrieval info: CBX_MODULE_PREFIX: ON
