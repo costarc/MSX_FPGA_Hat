0ffcfd7c3e92344eca4ff487b3ea3b3492a95317
Roni <ronivon.costa@gmail.com>
2024-04-19 21:44:13 +0100

    Added LICENSE files

--- Reference Designs/Altera DE0 Project/SDMapper/SDMapper.vhd
+++ Reference Designs/Altera DE0 Project/SDMapper/SDMapper.vhd
@@ -1,5 +1,28 @@
+-- --------------------------------------------------------------------------------------
 -- MSX_DE0/DE1 FPGA Interface
--- Ronivon Costa @ 2023
+-- --------------------------------------------------------------------------------------
+-- MIT License
+-- 
+-- Copyright (c) 2024 Ronivon Costa
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+-- 
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+-- 
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+-- --------------------------------------------------------------------------------------
 --
 -- MSXDOS2 (actually it has Nextor Operating System in the Flash) is one of the many Reference Designs
 -- I created for the MSX FPGA Interface.
